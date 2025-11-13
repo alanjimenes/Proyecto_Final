696094bb7618b4a4bbe50cf555c5e347739053a2
elian <elian@DESKTOP-EJ5530>
2025-11-13 16:22:42 -0400

    Cambios del login

--- Proyecto_Final/src/Visual/Login.java
+++ Proyecto_Final/src/Visual/Login.java
@@ -21,8 +21,17 @@ import javax.swing.JTextField;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.Color;
+import javax.swing.ImageIcon;
+import javax.swing.SwingConstants;
 
 public class Login extends JFrame {
+	public Login() {
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(Color.WHITE);
+		getContentPane().add(panel, BorderLayout.CENTER);
+	}
 
 	private JPanel contentPane;
 	private JTextField textField;
