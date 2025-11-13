5d4593b8ad1d41686c669a1d1168bceff370ca97
alanj <alanj@ASUS-AJ>
2025-11-13 08:30:04 -0400

    Se agrego la instancia a la clase clinica, se le coloco los gets a set y queda pendiente el realizar el arreglar la logica del login y hacer el visual......

--- Proyecto_Final/src/logico/Clinica.java
+++ Proyecto_Final/src/logico/Clinica.java
@@ -4,17 +4,17 @@ import java.util.ArrayList;
 
 public class Clinica {
 
+	private int genCodigoPaciente = 1;
+	private int genCodigoCita = 1;
+	private int genCodigoConsulta = 1;
 	private ArrayList<Paciente> pacientes;
 	private ArrayList<Medico> medicos;
 	private ArrayList<Enfermedad> enfermedades;
 	private ArrayList<Vacuna> vacunas;
 	private ArrayList<Cita> citas;
 
-	private int genCodigoPaciente = 1;
-	private int genCodigoCita = 1;
-	private int genCodigoConsulta = 1;
-
 	private static Medico medicoIniciado;
+	private static Clinica instancia;
 
 
 	public Clinica() {
@@ -26,13 +26,13 @@ public class Clinica {
 		medicoIniciado = null;
 	}
 
-	//FALTAN LOS GETTERS AND SETTERS
+
 
 	public static Clinica getInstance() {
-		if (instance == null) {
-			instance = new Clinica();
+		if (instancia == null) {
+			instancia = new Clinica();
 		}
-		return instance;
+		return instancia;
 	}
 
 	public void insertarPaciente(Paciente pac) {
@@ -52,6 +52,120 @@ public class Clinica {
 		return null;
 	}
 
+
+	public int getGenCodigoPaciente() {
+		return genCodigoPaciente;
+	}
+
+	public void setGenCodigoPaciente(int genCodigoPaciente) {
+		this.genCodigoPaciente = genCodigoPaciente;
+	}
+
+
+	public int getGenCodigoCita() {
+		return genCodigoCita;
+	}
+
+	public void setGenCodigoCita(int genCodigoCita) {
+		this.genCodigoCita = genCodigoCita;
+	}
+
+
+	public int getGenCodigoConsulta() {
+		return genCodigoConsulta;
+	}
+
+	public void setGenCodigoConsulta(int genCodigoConsulta) {
+		this.genCodigoConsulta = genCodigoConsulta;
+	}
+
+
+	public ArrayList<Paciente> getPacientes() {
+		return pacientes;
+	}
+
+
+
+	public void setPacientes(ArrayList<Paciente> pacientes) {
+		this.pacientes = pacientes;
+	}
+
+
+	public ArrayList<Medico> getMedicos() {
+		return medicos;
+	}
+
+
+	public void setMedicos(ArrayList<Medico> medicos) {
+		this.medicos = medicos;
+	}
+
+
+	public ArrayList<Enfermedad> getEnfermedades() {
+		return enfermedades;
+	}
+
+
+
+
+
+
+
+	public void setEnfermedades(ArrayList<Enfermedad> enfermedades) {
+		this.enfermedades = enfermedades;
+	}
+
+
+
+
+
+
+
+	public ArrayList<Vacuna> getVacunas() {
+		return vacunas;
+	}
+
+
+	public void setVacunas(ArrayList<Vacuna> vacunas) {
+		this.vacunas = vacunas;
+	}
+
+
+	public ArrayList<Cita> getCitas() {
+		return citas;
+	}
+
+
+	public void setCitas(ArrayList<Cita> citas) {
+		this.citas = citas;
+	}
+
+
+	public static Medico getMedicoIniciado() {
+		return medicoIniciado;
+	}
+
+
+	public static void setMedicoIniciado(Medico medicoIniciado) {
+		Clinica.medicoIniciado = medicoIniciado;
+	}
+
+
+
+
+	public static Clinica getInstancia() {
+		return instancia;
+
+	}
+
+
+	public static void setInstancia(Clinica instancia) {
+			Clinica.instancia = instancia;
+		}
+
+
+
+		/*
 	public Paciente buscarPacienteCedula(String cedula){
 
 	}
@@ -73,36 +187,37 @@ public class Clinica {
 	public void modificarCita(Cita cita) {
 
 	}
+		 */
+		//LOGIN
 
-	//LOGIN
-
-	public static Medico getMedicoLogueado() {
-		return medicoIniciado;
-	}
-
-	public static void logoutMedico() {
-		medicoIniciado = null;
-	}
-
-	/*Logica del login:
-	 * Para el usuario se usara la contraseña (menos jodon y mas sencillo ya que estara ingresado :)  )
-	 * Para la contraseña es obvio no?
-	 * 
-	 * Entonces el codigo retorna True si el medico inicio sesion correctamente
-	 * False si fallo
-	 * 
-	 * Fin de explicacion :)   
-	 * OJAZO AQUI: saben que falta insertarlo en lo visual no quise joder
-	 *  con eso ahora dejemos eso pa depue :)
-	 */
-	public boolean loginMedico(String cedula, String password) {
-		for (Medico medico : medicos) {
-			if (medico.getCedula().equals(cedula) && medico.getPassword().equals(password)) {
-				medicoIniciado = medico; 
-				return true;
-			}
+		public static Medico getMedicoLogueado() {
+			return medicoIniciado;
 		}
-		return false; 
-	}
 
-}
+		public static void logoutMedico() {
+			medicoIniciado = null;
+		}
+
+		/*Logica del login:
+		 * Para el usuario se usara la contraseña (menos jodon y mas sencillo ya que estara ingresado :)  )
+		 * Para la contraseña es obvio no?
+		 * 
+		 * Entonces el codigo retorna True si el medico inicio sesion correctamente
+		 * False si fallo
+		 * 
+		 * Fin de explicacion :)   
+		 * OJAZO AQUI: saben que falta insertarlo en lo visual no quise joder
+		 *  con eso ahora dejemos eso pa depue :)
+		 */
+		public boolean loginMedico(String cedula, String password) {
+			for (Medico medico : medicos) {
+				if (medico.getCedula().equals(cedula) && medico.getPassword().equals(password)) {
+					medicoIniciado = medico; 
+					return true;
+				}
+			}
+			return false; 
+		}
+
+
+	}
