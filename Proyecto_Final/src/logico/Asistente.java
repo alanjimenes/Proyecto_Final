6e10ab1b3145f1d4c24458f0f18af076359579ff
alanj <alanj@ASUS-AJ>
2025-11-14 18:01:42 -0400

    cambio de nombre de secretaria a asistente xd

--- Proyecto_Final/src/logico/Asistente.java
+++ Proyecto_Final/src/logico/Asistente.java
@@ -3,9 +3,9 @@ package logico;
 import java.time.LocalDate;
 
 
-public class Secretaria extends Persona {
+public class Asistente extends Persona {
 
-	public Secretaria(String cedula, String nombre, String apellido, LocalDate fechaNacimiento, String telefono,
+	public Asistente(String cedula, String nombre, String apellido, LocalDate fechaNacimiento, String telefono,
 			String direccion) {
 		super(cedula, nombre, apellido, fechaNacimiento, telefono, direccion);
 	}
