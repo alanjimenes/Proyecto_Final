f30c292453b64254e3e741e30613ec7b98f3f013
alanj <alanj@ASUS-AJ>
2025-11-14 17:32:59 -0400

    Cambio en metodos de buscar paciente, ahora en vez de buscarlo por cedula se busca por expediente

--- Proyecto_Final/src/logico/Medico.java
+++ Proyecto_Final/src/logico/Medico.java
@@ -12,15 +12,16 @@ public class Medico extends Persona implements Serializable {
 	private static final long serialVersionUID = 1L;
 	private Especialidad especialidad;
 	private int maxCitasPorDia;
-	private ArrayList<Consulta> consultasRealizadas;
-	 
+	private ArrayList<Consulta> consultasChecks;
+	private ArrayList<Cita> citasAsignadas; 
 
 	public Medico(String cedula, String nombre, String apellido, LocalDate fechaNacimiento, String telefono,
 			String direccion, Especialidad especialidad, int maxCitasPorDia, ArrayList<Consulta> consultasRealizadas, ArrayList<Cita> citasAsignadas) {
 		super(cedula, nombre, apellido, fechaNacimiento, telefono, direccion);
 		this.especialidad = especialidad;
 		this.maxCitasPorDia = maxCitasPorDia;
-		this.consultasRealizadas = new ArrayList<>();
+		this.consultasChecks = new ArrayList<>();
+		this.citasAsignadas = new ArrayList<>();
 	}
 
 	public Especialidad getEspecialidad() {
@@ -35,21 +36,35 @@ public class Medico extends Persona implements Serializable {
 		return maxCitasPorDia;
 	}
 
+
 	public void setMaxCitasPorDia(int maxCitasPorDia) {
 		this.maxCitasPorDia = maxCitasPorDia;
 	}
 
 	public ArrayList<Consulta> getConsultasRealizadas() {
-		return consultasRealizadas;
+		return consultasChecks;
 	}
 
 	public void setConsultasRealizadas(ArrayList<Consulta> consultasRealizadas) {
-		this.consultasRealizadas = consultasRealizadas;
+		this.consultasChecks = consultasRealizadas;
 	}
 
 	public void agregarConsultaRealizada(Consulta consulta) {
-		this.consultasRealizadas.add(consulta);
+		this.consultasChecks.add(consulta);
 	}
 
-	
+	public ArrayList<Cita> getCitasAsignadas() {
+		return citasAsignadas;
+	}
+
+	public void setCitasAsignadas(ArrayList<Cita> citasAsignadas) {
+		this.citasAsignadas = citasAsignadas;
+	}
+
+	public void agregarCitaAsignada(Cita c) {
+		this.citasAsignadas.add(c);
+	}
+
+
+
 }
