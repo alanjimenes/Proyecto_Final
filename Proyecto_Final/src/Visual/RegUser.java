6b3a1c386bf3e31c97c274459e588e252170a571
eduar <eduar@DESKTOP-5RA7CHC>
2025-11-14 20:17:57 -0400

    Creacion de RegUser (registrar Usuarios)

--- Proyecto_Final/src/Visual/RegUser.java
+++ Proyecto_Final/src/Visual/RegUser.java
@@ -1,6 +1,7 @@
 package Visual;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.FlowLayout;
 
 import javax.swing.JButton;
@@ -17,6 +18,9 @@ import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.ImageIcon;
+import java.awt.Font;
+import javax.swing.SwingConstants;
 
 public class RegUser extends JDialog {
 
@@ -48,43 +52,64 @@ public class RegUser extends JDialog {
 		getContentPane().setLayout(new BorderLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
-		contentPanel.setLayout(null);
+		contentPanel.setLayout(null); 
 
 		JLabel lblNombreUsuario = new JLabel("Nombre Usuario:");
-		lblNombreUsuario.setBounds(20, 26, 97, 14);
+		lblNombreUsuario.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblNombreUsuario.setForeground(Color.BLACK); 
+		lblNombreUsuario.setBounds(20, 26, 127, 14);
 		contentPanel.add(lblNombreUsuario);
 
 		textField = new JTextField();
-		textField.setBounds(20, 49, 127, 20);
+		textField.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		textField.setBounds(20, 65, 127, 20); // y=49
 		contentPanel.add(textField);
 		textField.setColumns(10);
 
-		comboBox = new JComboBox();
-		comboBox.setModel(new DefaultComboBoxModel(new String[] {"<Seleccione>", "Administrador", "Comercial"}));
-		comboBox.setBounds(20, 113, 127, 20);
-		contentPanel.add(comboBox);
-
-		JLabel lblTipo = new JLabel("Tipo:");
-		lblTipo.setBounds(20, 88, 97, 14);
-		contentPanel.add(lblTipo);
-
-		textField_1 = new JTextField();
-		textField_1.setBounds(190, 49, 147, 20);
-		contentPanel.add(textField_1);
-		textField_1.setColumns(10);
-
 		JLabel lblPassword = new JLabel("Password:");
-		lblPassword.setBounds(189, 26, 97, 14);
+		lblPassword.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblPassword.setForeground(Color.BLACK);
+		lblPassword.setBounds(20, 147, 97, 14);
 		contentPanel.add(lblPassword);
 
+		textField_1 = new JTextField();
+		textField_1.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		textField_1.setColumns(10);
+		textField_1.setBounds(20, 199, 147, 20); 
+		contentPanel.add(textField_1);
+
+		JLabel lblTipo = new JLabel("Tipo:");
+		lblTipo.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblTipo.setForeground(Color.BLACK);
+		lblTipo.setBounds(216, 23, 97, 20);
+		contentPanel.add(lblTipo);
+
+		comboBox = new JComboBox();
+		comboBox.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"<Seleccione>", "Administrador", "Asistente", "Medico"}));
+		comboBox.setBounds(216, 65, 127, 20); // y=113
+		contentPanel.add(comboBox);
+
 		JLabel lblConfirmarPassword = new JLabel("Confirmar Password:");
-		lblConfirmarPassword.setBounds(189, 88, 167, 14);
+		lblConfirmarPassword.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblConfirmarPassword.setForeground(Color.BLACK);
+		lblConfirmarPassword.setBounds(214, 147, 167, 14);
 		contentPanel.add(lblConfirmarPassword);
 
 		textField_2 = new JTextField();
+		textField_2.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		textField_2.setColumns(10);
-		textField_2.setBounds(190, 113, 147, 20);
+		textField_2.setBounds(216, 199, 147, 20);
 		contentPanel.add(textField_2);
+
+		JLabel lblFondoIcon = new JLabel("");
+		lblFondoIcon.setVerticalAlignment(SwingConstants.TOP);
+		lblFondoIcon.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblFondoIcon.setBackground(Color.WHITE);
+		lblFondoIcon.setIcon(new ImageIcon(RegUser.class.getResource("/icons/logo.png")));
+		lblFondoIcon.setBounds(-181, -160, 778, 533);
+		contentPanel.add(lblFondoIcon);
+
 		{
 			JPanel buttonPane = new JPanel();
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
@@ -95,8 +120,9 @@ public class RegUser extends JDialog {
 					public void actionPerformed(ActionEvent e) {
 						User user = new User(comboBox.getSelectedItem().toString(),textField.getText(),textField_1.getText());
 						Control.getInstance().regUser(user);
+						dispose();
 					}
-				}); 
+				});
 				okButton.setActionCommand("OK");
 				buttonPane.add(okButton);
 				getRootPane().setDefaultButton(okButton);
