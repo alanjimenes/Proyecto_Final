1e70813bfcb8bdf3d824f369f1a50d5b0545620f
eduar <eduar@DESKTOP-5RA7CHC>
2025-11-23 16:46:17 -0400

    Agregacion de la corrida del server a Main

--- Proyecto_Final/src/Visual/Main.java
+++ Proyecto_Final/src/Visual/Main.java
@@ -1,16 +1,31 @@
 package Visual;
 
 import java.awt.EventQueue;
+import Servidor.Server;
 
 public class Main {
 
 	public static void main(String[] args) {
+
+		Thread hiloServer = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					System.out.println("--- Iniciando Servidor Interno ---");
+					Server.main(null); 
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		hiloServer.start(); 
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
+					Thread.sleep(1000); 
+
 					Login frame = new Login();
 					frame.setVisible(true);
-
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
